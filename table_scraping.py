66fa7879b1654723e3a0c3f63adef0fbeb423221
Arpik <arpiksargsyan@gmail.com>
2019-11-28 21:34:37 +0400

    Added get_pokemon_links() function. It returns the list of all the links from td's <a> tag.

--- table_scraping.py
+++ table_scraping.py
@@ -16,3 +16,15 @@ def get_pokemon_name():
     
     return pok_names
 
+def get_pokemon_links():
+    request = requests.get('https://pokemondb.net/pokedex/all')
+    soup = bs4.BeautifulSoup(request.text, 'lxml')
+    table = soup.find('table', {'id' : 'pokedex'})
+    tbody_rows = table.tbody.find_all('tr')
+    pok_links = []
+    for i in range(0,10): 
+        pok_link = tbody_rows[i].find_all('td', class_='cell-name')[0].find('a')['href']
+        pok_links.append(pok_link)
+
+    print(pok_links)
+    return pok_links
