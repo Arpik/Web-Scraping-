69fe5af0d1753fea7b79cd378944bbd9e44ace7c
Arpik <arpiksargsyan@gmail.com>
2019-12-03 15:08:11 +0400

    Parsed the html with BeautifulSoup.

--- turnstile/turnstile.py
+++ turnstile/turnstile.py
@@ -4,8 +4,11 @@
 import requests
 import urllib.request
 import time
+import lxml
 from bs4 import BeautifulSoup
 
 url = 'http://web.mta.info/developers/turnstile.html'
 response = requests.get(url)
+# print(response)
 
+soup = BeautifulSoup(response.text, "lxml")
