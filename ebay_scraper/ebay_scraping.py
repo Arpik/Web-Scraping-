4981ee60bdb35ba3350ab1749ab13a93bc0ea159
Arpik <arpiksargsyan@gmail.com>
2019-12-17 15:45:12 +0400

    Added main() funciton.

--- ebay_scraper/ebay_scraping.py
+++ ebay_scraper/ebay_scraping.py
@@ -4,3 +4,11 @@
 # 3. Collect all links to detail pages of each product. 
 # 4. Write scraped data to a csv file.
 
+# main() function will manage the calls of other functions and will collect scraped data.
+def main():
+    pass
+
+
+# Checks if ebay_scraping.py was run directly from the console or not.
+if __name__ = '__main__':
+    main()
