6b9f9e7fe6016c98fc9966d314722e88ab5cd374
Arpik <arpiksargsyan@gmail.com>
2019-12-17 15:35:25 +0400

    Added task description.

---
+++ ebay_scraper/ebay_scraping.py
@@ -0,0 +1,6 @@
+# TODO
+# 1. Make a request to the ebay.com.
+# 2. Collect data from each detail page.
+# 3. Collect all links to detail pages of each product. 
+# 4. Write scraped data to a csv file.
+
